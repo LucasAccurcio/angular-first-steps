d5b8670877c5b7734ef3a8a1fe3477ef4da3d0ab
LucasAccurcio <lucas.accurcio@gmail.com>
2022-11-22 21:10:52 -0300

    feat: :sparkles: add routes to the application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const routes: Routes = [{
+  path: 'about',
+  component: AboutComponent
+},
+{
+  path: 'contact',
+  component: ContactComponent
+},
+{
+  path: 'home',
+  component: HomeComponent
+},
+{
+  path: 'login',
+  component: LoginComponent
+},
+{
+  path: '',
+  redirectTo: '/home',
+  pathMatch: 'full'
+},
+{
+  path: '**',
+  component: PageNotFoundComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
